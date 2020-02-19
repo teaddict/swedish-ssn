3e5a5133ebe719d23701bc0bc45731abc467b7c7
Romāns Djatlovs <romans.djatlovs@sapiens.com>
2020-02-19 12:52:13 +0200

    Change to swedish locale 'sv-SE' Update build and minify scripts Fix linter issues Update babel

--- dist/swedish-ssn.js
+++ dist/swedish-ssn.js
@@ -1,6 +1,6 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define('SwedishSSN', ['module', 'exports'], factory);
+    define("SwedishSSN", ["module", "exports"], factory);
   } else if (typeof exports !== "undefined") {
     factory(module, exports);
   } else {
@@ -17,7 +17,6 @@
    * Purpose: Validate and generate Swedish SSN's according to https://en.wikipedia.org/wiki/Personal_identity_number_(Sweden)
    * Author:  teaddict
    */
-
   //! https://github.com/teaddict/swedish-ssn | Version: 1.0.0
 
   Object.defineProperty(exports, "__esModule", {
@@ -30,23 +29,21 @@
     }
   }
 
-  var _createClass = function () {
-    function defineProperties(target, props) {
-      for (var i = 0; i < props.length; i++) {
-        var descriptor = props[i];
-        descriptor.enumerable = descriptor.enumerable || false;
-        descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
-        Object.defineProperty(target, descriptor.key, descriptor);
-      }
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
     }
+  }
 
-    return function (Constructor, protoProps, staticProps) {
-      if (protoProps) defineProperties(Constructor.prototype, protoProps);
-      if (staticProps) defineProperties(Constructor, staticProps);
-      return Constructor;
-    };
-  }();
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
 
   var SwedishSSN = function () {
     function SwedishSSN() {
@@ -54,18 +51,21 @@
     }
 
     _createClass(SwedishSSN, null, [{
-      key: 'validate',
+      key: "validate",
       value: function validate(ssn) {
         if (ssn === undefined || ssn === null) {
           return false;
         }
 
         var ssnAsArray = parse(ssn);
+
         if (ssnAsArray.length === 10) {
           var checkDigit = ssnAsArray.pop();
+
           switch (getChecksum(ssnAsArray)) {
             case checkDigit:
               return true;
+
             default:
               return false;
           }
@@ -74,7 +74,7 @@
         }
       }
     }, {
-      key: 'generateRandomSSN',
+      key: "generateRandomSSN",
       value: function generateRandomSSN() {
         var birthdate = getRandomDate();
         var secondPart = getRandomNumber(1000, 9998, 'random');
@@ -85,7 +85,7 @@
         return ssnFormatter(ssnAsArray);
       }
     }, {
-      key: 'generateSSNWithParameters',
+      key: "generateSSNWithParameters",
       value: function generateSSNWithParameters(birthdate, gender) {
         var secondPart = getRandomNumber(1000, 9999, gender);
         var randomSsn = yymmdd(birthdate) + secondPart;
@@ -100,43 +100,53 @@
   }();
 
   exports.default = SwedishSSN;
-
-
   var genderEnum = {
     MALE: 'male',
     FEMALE: 'female'
   };
 
   function yymmdd(birthdate) {
-    var formatted = birthdate.toLocaleDateString("en-CA", { year: "2-digit", month: "2-digit", day: "2-digit" }).replace(/\D/g, "");
+    var formatted = birthdate.toLocaleDateString("sv-SE", {
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit"
+    }).replace(/\D/g, "");
     return formatted;
   }
-
   /**
   *returns a random number as string for second part of ssn
   */
+
+
   function getRandomNumber(min, max, gender) {
     var number = Math.floor(Math.random() * (max - min + 1) + min);
+
     switch (gender) {
       case genderEnum.MALE:
         if (isEven(number)) {
           number = number + 1;
         }
+
         break;
+
       case genderEnum.FEMALE:
         if (isOdd(number)) {
           number = number + 1;
         }
+
         break;
+
       default:
         break;
     }
+
     return number.toString().substring(1);
   }
 
   function isEven(x) {
     return x % 2 === 0;
   }
+
   function isOdd(x) {
     return !isEven(x);
   }
@@ -161,6 +171,7 @@
     var reducer = function reducer(accumulator, currentValue) {
       return accumulator + currentValue;
     };
+
     var sumOfDigits = digits.reduce(reducer);
     var checksum = Math.floor(sumOfDigits * 9 % 10);
     return checksum;
@@ -169,27 +180,32 @@
   function sumOfNum(num) {
     return Math.floor(num % 10) + Math.floor(num / 10);
   }
-
   /**
   * returns a formatted SSN 'yymmdd-fjkm'
   */
+
+
   function ssnFormatter(ssnAsArray) {
     var formattedSSN = ssnAsArray.slice(0, 6).join('').toString() + "-" + ssnAsArray.slice(6, 10).join('').toString();
     return formattedSSN;
   }
-
   /**
    * Parse parameter given SSN string. Remove all characters except digits.
    * @param ssn - {String} SSN to parse
    * @returns Int[]
    */
+
+
   function parse(ssn) {
     var cleaned = ssn.replace(/\D/g, "").split("");
+
     if (cleaned.length === 12) {
       return cleaned.slice(2, 12).map(Number);
     }
+
     return cleaned.map(Number);
   }
-  module.exports = exports['default'];
+
+  module.exports = exports["default"];
 });
 
